1af435194f42d470e60f1f6ab8b1efd7ab803098
JorgeChaparroS <jorgechaparrosarmiento@gmail.com>
2023-05-14 18:42:41 -0500

    Author

--- src/app/containers/author/author.module.ts
+++ src/app/containers/author/author.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthorComponent } from './author.component';
 import { AuthorRoutingModule } from './author-routing.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [AuthorComponent],
   imports: [
     CommonModule,
-    AuthorRoutingModule
+    AuthorRoutingModule,
+    TranslateModule
   ]
 })
 export class AuthorModule { }
